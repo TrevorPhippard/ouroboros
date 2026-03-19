f2845a65313b00e686cd53141404ed7332d1c157
Trevor Phippard <trevorphippard@ea:8d:f0:bc:24:9b.home>
2026-03-19 10:16:10 -0400

    fix cors issue

--- services/api-gateway/go.mod
+++ services/api-gateway/go.mod
@@ -4,7 +4,9 @@ go 1.25.3
 
 require (
 	github.com/99designs/gqlgen v0.17.88
+	github.com/rs/cors v1.11.1
 	github.com/vektah/gqlparser/v2 v2.5.32
+	google.golang.org/grpc v1.79.3
 )
 
 require (
@@ -14,5 +16,10 @@ require (
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/sosodev/duration v1.4.0 // indirect
+	golang.org/x/net v0.50.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 )
--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/rs/cors" // Import the CORS package
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
@@ -26,7 +27,8 @@ func main() {
 	defer userConn.Close()
 
 	// Connect to Todo Service
-	todoConn, err := grpc.Dial("localhost:50053", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	todoConn, err := grpc.NewClient("localhost:50053", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
 	if err != nil {
 		log.Fatalf("could not connect to todo-service: %v", err)
 	}
@@ -40,8 +42,20 @@ func main() {
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: resolver}))
 
+// Configure CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"}, // Allow your Next.js app
+		AllowCredentials: true,                              // Required if you are sending cookies/sessions
+		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		Debug:            false, // Set to true if you need to debug CORS headers in your Go console
+	})
+
+// Wrap the GraphQL server handler with the CORS middleware
+graphqlHandler := c.Handler(srv)
+
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", graphqlHandler)
 
 	log.Printf("Gateway live at http://localhost:%s/", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
