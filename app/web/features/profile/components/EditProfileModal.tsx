430b8922d95dfdd18da1b28e33ab120a9e0ebafd
Trevor Phippard <trevorphippard@9a:f2:58:b5:ba:ad.home>
2026-04-11 13:29:17 -0400

    dev

--- app/web/features/profile/components/EditProfileModal.tsx
+++ app/web/features/profile/components/EditProfileModal.tsx
@@ -3,7 +3,7 @@
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Camera, X } from "lucide-react"
-import { profileSchema, ProfileFormValues } from "../schemas"
+import { ProfileType, profileSchema } from "../schemas"
 import { useUpdateProfile } from "../hooks/useUpdateProfile"
 import { useState } from "react"
 
@@ -11,7 +11,7 @@ export const EditProfileModal = ({
   initialData,
   onClose,
 }: {
-  initialData: any
+  initialData: ProfileType
   onClose: () => void
 }) => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null)
@@ -21,13 +21,13 @@ export const EditProfileModal = ({
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<ProfileFormValues>({
+  } = useForm<ProfileType>({
     resolver: zodResolver(profileSchema),
     defaultValues: initialData,
   })
 
-  const onSubmit = async (data: ProfileFormValues) => {
-    await updateProfile({ data, avatarFile: selectedFile || undefined })
+  const onSubmit = async (data: ProfileType) => {
+    await updateProfile({  avatarFile: selectedFile || undefined })
     onClose()
   }
 
